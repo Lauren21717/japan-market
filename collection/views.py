035532b55211d3b254f9cb766db6aaa5aaa248d9
Lauren Yeung <laurenyeung217@gmail.com>
2024-10-25 20:41:59 +0000

    Feat: Add collection view and update URL configurations.

--- collection/views.py
+++ collection/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+def collection(request):
+    """
+    A view to show all products, including sorting and search queries
+    """
+    
+    products = Product.objects.all()
+    
+    context = {
+        'products': products,
+    }
+    
+    return render(request, 'collection/collection.html', context)
