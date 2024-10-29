524970522b7025f265d962aaee2ab5636fea6901
Lauren Yeung <laurenyeung217@gmail.com>
2024-10-29 20:06:54 +0000

    Added 'adjust_bag' view to update or remove items from the cart session.

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import (
+    render, redirect, HttpResponse, reverse, get_object_or_404
+    )
 from django.contrib import messages
 from collection.models import Product
 
@@ -38,3 +40,35 @@ def add_to_bag(request, item_id):
     request.session['bag'] = bag
     messages.success(request, f'Added {product.name} to your bag')
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    size = request.POST.get('product_size', None)
+    bag = request.session.get('bag', {})
+
+    if size:
+        if item_id in bag:
+            if 'items_by_size' in bag[item_id]:
+                if quantity > 0:
+                    bag[item_id]['items_by_size'][size] = quantity
+                else:
+                    del bag[item_id]['items_by_size'][size]
+                    if not bag[item_id]['items_by_size']:
+                        bag.pop(item_id)
+            else:
+                bag[item_id]['items_by_size'] = {size: quantity}
+    else:
+        if quantity > 0:
+            bag[item_id] = quantity
+        else:
+            bag.pop(item_id, None)
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+
+
