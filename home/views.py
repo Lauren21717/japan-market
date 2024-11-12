876f429246714f16f53b94800ae2dada62c976e0
Lauren Yeung <laurenyeung217@gmail.com>
2024-11-12 13:08:23 +0000

    Add Special Offer product display to home page

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from collection.models import Product, Category
+from django.db.models import Q
 
 # Create your views here.
 
@@ -8,7 +9,13 @@ def index(request):
     A view to return the home page
     """
     categories = Category.objects.exclude(name='all_collections')
+
+    products = Product.objects.filter(
+        Q(is_special_offer=True) | Q(is_featured=True)
+    )[:4]
+
     context = {
         'categories': categories,
+        'products': products,
     }
     return render(request, 'home/index.html', context)
