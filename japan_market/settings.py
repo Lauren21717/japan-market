f9b57987e2c6edd87ad6a2e31a99589261a1344d
whitecatbb <laurenyeung217@gmail.com>
2025-02-24 03:07:21 +0000

    Enforce HTTPS for AWS S3 URLs

--- japan_market/settings.py
+++ japan_market/settings.py
@@ -181,6 +181,8 @@ if 'USE_AWS' in os.environ:
         'CacheControl': 'max-age=94608000',
     }
 
+    AWS_S3_URL_PROTOCOL = 'https:'
+
     STORAGES = {
         'default': {'BACKEND': 'storages.backends.s3boto3.S3Boto3Storage'},
         'staticfiles': {'BACKEND': 'storages.backends.s3boto3.S3Boto3Storage', 'OPTIONS': {'location': 'static'}},
